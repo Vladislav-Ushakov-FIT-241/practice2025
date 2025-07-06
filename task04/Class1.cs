1f15ec6b824ed32af8c50f1e4d479481aa3477d7
Vladislav-Ushakov-FIT-241 <chesmotrishotvernis@gmail.com>
2025-07-06 16:23:03 +0600

    task04

---
+++ task04/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace task04
+{
+    public interface ISpaceship
+    {
+        void MoveForward();      // Движение вперед
+        void Rotate(int angle);  // Поворот на угол (градусы)
+        void Fire();             // Выстрел ракетой
+        int Speed { get; }       // Скорость корабля
+        int FirePower { get; }   // Мощность выстрела
+    }
+
+    public class Cruiser : ISpaceship
+    {
+        public int Speed { get; } = 50;
+        public int FirePower { get; } = 100;
+        public int Position { get; private set; } = 0;
+        public int Angle { get; private set; } = 0;
+        public int Ammo { get; private set; } = 4;
+
+        public void MoveForward()
+        {
+            Position += Speed;
+        }
+
+        public void Rotate(int angle)
+        {
+            Angle = (Angle + angle) % 360;
+        }
+
+        public void Fire()
+        {
+            if (Ammo > 0) Ammo--;
+        }
+    }
+
+    public class Fighter : ISpaceship
+    {
+        public int Speed { get; } = 100;
+        public int FirePower { get; } = 20;
+        public int Position { get; private set; } = 0;
+        public int Angle { get; private set; } = 0;
+        public int Ammo { get; private set; } = 20;
+
+        public void MoveForward()
+        {
+            Position += Speed;
+        }
+
+        public void Rotate(int angle)
+        {
+            Angle = (Angle + angle) % 360;
+        }
+
+        public void Fire()
+        {
+            if (Ammo > 0) Ammo--;
+        }
+    }
+}
