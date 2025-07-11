e7fd65f8d6ee42405a0adbce9e481ca6d1b1362b
Vladislav-Ushakov-FIT-241 <chesmotrishotvernis@gmail.com>
2025-07-11 15:00:34 +0600

    task09

---
+++ task09/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+
+namespace task09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFrom(args[0]);
+
+            Array.ForEach(assembly.GetTypes(), type =>
+            {
+                Console.WriteLine($"Класс: {type.Name}");
+
+                Array.ForEach((Attribute[])type.GetCustomAttributes(),
+                    attr => Console.WriteLine($"Атрибут: {attr.GetType().Name}"));
+
+                Array.ForEach(type.GetConstructors(), constructor =>
+                {
+                    Console.WriteLine($"Конструктор: {constructor.Name}");
+                    Array.ForEach(constructor.GetParameters(),
+                        param => Console.WriteLine($"Параметр: {param.ParameterType.Name} {param.Name}"));
+                });
+
+                Array.ForEach(type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly), method =>
+                {
+                    Console.WriteLine($"Метод: {method.Name}");
+                    Array.ForEach(method.GetParameters(),
+                        param => Console.WriteLine($"Параметр: {param.ParameterType.Name} {param.Name}"));
+                });
+            });
+        }
+    }
+}
